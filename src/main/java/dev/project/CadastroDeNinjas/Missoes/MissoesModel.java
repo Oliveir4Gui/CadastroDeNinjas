6eccfb45ea2b06bd2982cb2db957a7fb39ae9cbb
Guimg <guilherme.martins20@hotmail.com>
2025-07-16 23:29:24 -0300

    feature: Adiciona o Lombok nas classes para abstracao

--- src/main/java/dev/project/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/project/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,7 +3,13 @@ package dev.project.CadastroDeNinjas.Missoes;
 
 import dev.project.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 @Table(name = "tb_missoes")
 public class MissoesModel {
@@ -19,54 +25,4 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     private NinjaModel ninja;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nome, String dificuldade, NinjaModel ninja) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-        this.ninja = ninja;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public NinjaModel getNinja() {
-        return ninja;
-    }
-
-    public void setNinja(NinjaModel ninja) {
-        this.ninja = ninja;
-    }
-
-    @Override
-    public String toString() {
-        return "MissoesModel{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", dificuldade='" + dificuldade + '\'' +
-                ", ninja=" + ninja +
-                '}';
-    }
 }
--- src/main/java/dev/project/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/project/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,10 +2,16 @@ package dev.project.CadastroDeNinjas.Ninjas;
 
 import dev.project.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "tb_cadastro")
 public class NinjaModel {
@@ -24,43 +30,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") //Foreing Key
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public NinjaModel(String nome, String email, int idade, MissoesModel missoes) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-        this.missoes = missoes;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
