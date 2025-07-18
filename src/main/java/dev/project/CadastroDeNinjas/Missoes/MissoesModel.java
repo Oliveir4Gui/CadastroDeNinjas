028687060e65afefe067b8159081d8aef4cb62a7
Guimg <guilherme.martins20@hotmail.com>
2025-07-18 09:23:18 -0300

    fix: Adiciona @Column nas classes

--- src/main/java/dev/project/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/project/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -16,10 +16,13 @@ public class MissoesModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
 
+    @Column(name = "dificuldade")
     private String dificuldade;
 
     @OneToMany(mappedBy = "missoes")
--- src/main/java/dev/project/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/project/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -18,13 +18,20 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     Long id;
 
+    @Column(name = "nome")
     private String nome;
 
-    @Column(unique = true)
+
+    @Column(unique = true , name = "email")
     private String email;
 
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "idade")
     private int idade;
 
     @ManyToOne
