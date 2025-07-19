dc22009e07da9476ef4f15b49dcb277536365578
Guimg <guilherme.martins20@hotmail.com>
2025-07-19 13:19:34 -0300

    adiciona /data no gitignore

--- src/main/java/dev/project/CadastroDeNinjas/Missoes/MissoesRepository.java
+++ src/main/java/dev/project/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -1,6 +1,8 @@
 package dev.project.CadastroDeNinjas.Missoes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MissoesRepository extends JpaRepository<MissoesModel,Long> {
 }
