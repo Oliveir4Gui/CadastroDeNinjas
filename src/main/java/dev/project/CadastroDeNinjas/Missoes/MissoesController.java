f24764c345f52e5d878e78380c9050c0f226025c
Guimg <guilherme.martins20@hotmail.com>
2025-07-20 19:56:06 -0300

    feat: Adiciona a logica do service nas missoes

--- src/main/java/dev/project/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/project/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,10 +1,35 @@
 package dev.project.CadastroDeNinjas.Missoes;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/missoes")
 public class MissoesController {
+    @Autowired
+    private MissoesService missoesService;
+
+    @GetMapping("/listar")
+    public List<MissoesModel> listar(){
+        return missoesService.mostrarMissoes();
+    }
+
+    @PostMapping("/criar")
+    public MissoesModel criaMissao(@RequestBody MissoesModel missao){
+       return missoesService.criaMissao(missao);
+    }
+
+    @DeleteMapping("/excluir/{id}")
+    public void deletaNinjaId(@PathVariable long id){
+        missoesService.deletaMissaoId(id);
+    }
+
+    @PutMapping("/alterar/{id}")
+    public MissoesModel alteraMissao(@PathVariable long id , @RequestBody MissoesModel missao){
+        return missoesService.alteraMissaoId(id,missao);
+    }
+
 }
